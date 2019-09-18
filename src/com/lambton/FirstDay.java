22d4acfb2c25a2eadce98e020e54b67aac264bf9
shiji703 <shijieugin3@gmail.com>
2019-09-18 19:21:03 -0400

    [shiji] comment added

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -5,11 +5,14 @@ public class FirstDay
     public static void main(String[] args)
     {
         System.out.println ("shiji");
-
+        //variable declaration
         int a, b, c;
+        //value assignment
         a=10;
         b=20;
+        //perform add
         c = a + b;
+        //display
         System.out.println("sum : " + c);
     }
 }
